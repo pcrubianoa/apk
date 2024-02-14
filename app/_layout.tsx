dd7345794e527ff58eedaa56ee8816870300a3f5
Pablo Rubiano <pcrubianoa@users.noreply.github.com>
2024-02-14 16:21:48 -0500

    Drawer

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,58 +1,96 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Drawer } from 'expo-router/drawer';
+import { DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
+import { Feather, EvilIcons, SimpleLineIcons  } from '@expo/vector-icons';
+import { router, usePathname } from 'expo-router';
 
-import { useColorScheme } from '@/components/useColorScheme';
-
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+const CustomDrawerContent = (props:any) => {
+  const pathname = usePathname();
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
+  }, [pathname]);
+
+  return(
+    <DrawerContentScrollView {...props}>
+      <View>
+        <Text>Perfil</Text>
+      </View>
+    <DrawerItem
+    icon={(color, size) => (
+      <SimpleLineIcons name="people" size={24} color={pathname == '/two' ? '#fff' : '#000'} />
+    )}
+    label={'Clientes'}
+    labelStyle={[styles.navItemLabel, {color: pathname == '/two' ? '#fff' : '#000'}]}
+    style={{ backgroundColor: pathname == '/two' ? '#000' : '#fff' }}
+    onPress={() => {
+      router.push('/(drawer)/(tabs)/two');
+    }}
+    />
+    <DrawerItem
+    icon={(color, size) => (
+      <SimpleLineIcons  name="tag" size={24} color={color}/>
+    )}
+    label={'Productos'}
+    onPress={() => {
+      router.push('/(drawer)/(tabs)/');
+    }}
+    />
+    <DrawerItem
+    icon={(color, size) => (
+      <SimpleLineIcons name="user" size={24} color={color}/>
+    )}
+    label={'Perfil'}
+    onPress={() => {
+      router.push('/(drawer)/(tabs)/');
+    }}
+    />
+    <DrawerItem
+    icon={(color, size) => (
+      <SimpleLineIcons name="info" size={24} color={color}/>
+    )}
+    label={'Cambios'}
+    onPress={() => {
+      router.push('/(drawer)/(tabs)/');
+    }}
+    />
+    <DrawerItem
+    icon={(color, size) => (
+      <SimpleLineIcons name="support" size={24} color={color}/>
+    )}
+    label={'Términos de Servicio'}
+    onPress={() => {
+      router.push('/(drawer)/(tabs)/');
+    }}
+    />
+    <DrawerItem
+    icon={(color, size) => (
+      <SimpleLineIcons name="power" size={24} color={color}/>
+    )}
+    label={'Cerrar Sesión'}
+    onPress={() => {
+      router.push('/(drawer)/(tabs)/');
+    }}
+    />
+    <View>
+      <Text>Logis</Text>
+      <Text>2024</Text>
+    </View>
+    </DrawerContentScrollView>
+  )
+
+}
+
+export default function Layout() {
+  return(
+    <Drawer drawerContent={(props:any) => <CustomDrawerContent {...props} />} />
+  )
+}
+
+const styles = StyleSheet.create({
+  navItemLabel: {
+    //marginLeft: -20,
+    //fontSize: 18
   }
-
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
-  );
-}
+});
